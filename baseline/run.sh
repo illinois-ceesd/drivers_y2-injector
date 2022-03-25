8092208957221db745cb6a8758e572d9d2d7e881
Mike Anderson <anderson2981@gmail.com>
2022-03-25 09:59:57 -0500

    added the mesh

--- baseline/run.sh
+++ baseline/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #mpirun -n 1 python -u -O -m mpi4py injector.py -i run_params.yaml --log --lazy
 #mpirun -n 1 python -u -O -m mpi4py injector.py -i run_params.yaml --log --lazy --overintegration
-mpirun -n 1 python -u -O -m mpi4py injector.py -i run_params.yaml --log
+mpirun -n 1 python -u -m mpi4py injector.py -i run_params.yaml --log
